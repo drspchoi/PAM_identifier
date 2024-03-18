517e02d1613109b3c61fbafb4220977c68de22a1
drspchoi <drspchoi@gmail.com>
2024-03-18 13:54:37 -0700

    data url was made. Uplaod and analyze seperated. Need to make select. I don't know why analyze didn't work.

---
+++ PAM_analyze.py
@@ -0,0 +1,61 @@
+def analyze_single_file(DNA_sequence, PAM_option):
+    
+    NA_sequence = ''.join(DNA_sequence).replace('\n', '')
+    Reverse_strand = ReverseStrandGenerator(DNA_sequence)
+
+    PAM_forward = PAM_identifier(DNA_sequence, PAM_option,"forward")
+    PAM_reverse = PAM_identifier(DNA_sequence, PAM_option,"backward")
+    return PAM_forward, PAM_reverse 
+
+def ReverseStrandGenerator(DNA_sequence):
+    Reverse_strand = ""
+    for x in DNA_sequence:
+        if x == 'a':
+            Reverse_strand += 't'
+        elif x == 't':
+            Reverse_strand += 'a'
+        elif x == 'g':
+            Reverse_strand += 'c'
+        elif x == 'c':
+            Reverse_strand += 'g'
+        else:
+            Reverse_strand += x
+    return Reverse_strand
+
+def PAM_identifier(DNA_sequence, PAM_option, direction):
+    combined_results={'PAM_position':[], 'PAM+protospacer':[], 'G+C (%)':[]}
+    final_result={}
+
+    if direction=="forward":
+        input_sequence = list(DNA_sequence)
+
+    elif direction=="backward":
+        DNA_sequence=ReverseStrandGenerator(DNA_sequence)
+        input_sequence = list(DNA_sequence)
+        PAM_option=[PAM[::-1] for PAM in PAM_option]
+        
+    for PAM in PAM_option:
+        PAM_position = DNA_sequence.find(PAM)
+
+        if direction=="backward" and PAM_position < 21:
+            continue   
+
+        while PAM_position != -1:
+            protospacer = DNA_sequence[PAM_position: PAM_position + 25] if direction =="forward" else DNA_sequence[PAM_position-21:PAM_position+4]
+            GC = (protospacer.count('g') + protospacer.count('c')) / len(protospacer) * 100
+            combined_results['PAM_position'].append(PAM_position + 1 if direction=="forward" else PAM_position +4)
+            combined_results['PAM+protospacer'].append(protospacer if direction=="forward" else protospacer[::-1])
+            combined_results['G+C (%)'].append(GC)
+
+            input_sequence[PAM_position: PAM_position + 4] = [
+                    '<strong style="color: red;">' + input_sequence[PAM_position],
+                    input_sequence[PAM_position + 1],
+                    input_sequence[PAM_position + 2],
+                    input_sequence[PAM_position + 3] + '</strong>']
+
+            PAM_position = DNA_sequence.find(PAM, PAM_position + 1)
+
+        final_result[PAM]=combined_results
+        combined_results={'PAM_position':[],'PAM+protospacer':[],'G+C (%)':[]}
+    
+    return final_result, ''.join(input_sequence)
--- index3.py
+++ index3.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 from sqlalchemy import create_engine,text
+from PAM_analyze import analyze_single_file
 
 
 app=Flask(__name__)
 engine=create_engine("mysql+pymysql://root:password@localhost/test")
 
 connection = engine.connect()
+def databaseConnect(engine):
+    connection = engine.connect()
+    result=connection.execute(text("select * from test2"))
+    rows=result.mappings()
 
+    return rows
 
 @app.route('/')
 def index():
-    result=connection.execute(text("select * from test"))
-
-    rows=result.mappings()
+    rows=databaseConnect(engine)
     
-    return render_template('index.html',result_content=[row['sequence'] for row in rows])
+    return render_template('index.html',result_content=rows)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -25,16 +29,35 @@ def upload():
 
     if file.filename == '':
         return "No selected file"
-    
-    file_name=file.filename
-    content=file.read()
-    content=str(content, 'utf-8')
-    print(content)
-    connection.execute(text(f"insert into test (id, name, sequence) values (3, '{file_name}', '{content}')"))
-    connection.commit()
 
-    return render_template('index.html')
+    file_name=file.filename
+    sequence=file.read()
+    sequence=str(sequence, 'utf-8')
+    sequence=''.join(sequence).replace('\r\n', '')
     
+    connection.execute(text(f"insert into test2 (name, sequence) values ('{file_name}', '{sequence}')"))
+    connection.commit()
+    
+    rows=databaseConnect(engine)
+
+    return render_template('index.html', result_content=rows)
+
+# why analysis doesn't work?
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    PAM_option=request.form.get('num').split(',')
+    sequence=connection.execute(text("select sequence from test2 where id=1"))
+    sequence=sequence.mappings()
+    print(sequence)
+    PAM_forward,PAM_reverse=analyze_single_file(sequence,PAM_option)
+    print(PAM_forward)
+    return render_template('index.html', result_content1=PAM_forward, result_content2=PAM_reverse)
+
+@app.route('/data')
+def databank():
+    rows=databaseConnect(engine)
+    return render_template('result.html', result_content=[row for row in rows]) #why I cannot jasonify this result content?
+
 if __name__=='__main__':
     app.run(debug=True)
 
